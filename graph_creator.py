cea21df8762d2b3b0aca514aa020e71568950c20
MONIER-ChE <monier.che@gmail.com>
2020-10-08 10:08:43 +0200

    significative_braket

---
+++ graph_creator.py
@@ -0,0 +1,307 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+## Plot style
+plt.style.use('bmh') ## ggplot
+
+## Global matplotlib param Edit the font, font size, and axes width
+plt.rcParams['font.size'] = 18
+plt.rcParams['axes.linewidth'] = 2
+plt.rcParams['lines.linewidth'] = 1.5
+
+## Legend
+plt.rcParams['legend.facecolor'] = '#FFFAFA'
+
+## Grid
+plt.rcParams['axes.grid'] = True# display grid or not
+plt.rcParams['axes.grid.axis'] = 'y'## which axis the grid should apply to
+
+## to define
+plt.rcParams['axes.labelsize']= 8
+plt.rcParams['legend.fontsize']= 10
+plt.rcParams['xtick.labelsize']= 10
+plt.rcParams['ytick.labelsize']= 10
+plt.rcParams['text.usetex']= False
+plt.rcParams['figure.figsize']= [4.5, 4.5]
+
+plt.rcParams['figure.figsize'] = 5, 10
+
+'''
+step
+
+for bar in chart (working for 1, 2, 3 groupin plot (pre/post training | base, pre, post))
+'''
+
+
+
+
+def grouped_bar_plot(a_mean, b_mean, a_std, b_std, groups_labels, variables_labels, grouped_describe):
+    '''
+    function for build grouped bar plot 
+    '''
+
+    x = np.arange(len(variables_labels))  # the label locations
+    width = 0.25  # the width of the bars
+
+    fig, ax = plt.subplots()
+    
+    a_bar = ax.bar(x - width/2, 
+                    a_mean, width, 
+                    label=groups_labels[0], 
+                    yerr=a_std, 
+                    align='center', 
+                    capsize = 10, 
+                    alpha=0.8) #capsize = limit to error bar
+
+                    
+
+    b_bar = ax.bar(x + width/2, 
+                    b_mean, width, 
+                    label=groups_labels[1], 
+                    yerr=b_std, 
+                    align='center', 
+                    capsize = 10,
+                    alpha=0.8) # trasnparency
+
+    
+
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    #ax.set_ylabel('Values')
+    #ax.set_xlabel('Variables')
+    ax.set_title('Title to define')
+    ax.set_xticks(x)
+    ax.set_xticklabels(variables_labels)
+    ax.legend()
+
+
+    
+    def statistic(bars, variables_labels):
+        '''
+        Create data of position
+        '''
+        for var in variables_labels:
+            
+            grouped_describe[var, 'x_pos'] = ['NaN','NaN']
+
+
+        a = 0
+        ## Find each values for group A then group B
+        for bar in bars:
+            pos = []
+            
+            for data in bar:
+                pos.append(data.get_x()+0.25/2) # mid width size
+                
+            
+            i=0
+            ## Fill values with good variables
+            for var in variables_labels:
+                
+                grouped_describe[var,'x_pos'][a] = pos[i]
+                i+=1
+            a +=1
+            
+
+    
+    def draw(label, grouped_describe):
+        '''
+        create braket stats
+        '''
+
+        ylim = max(grouped_describe[label,'max'].iloc[0], grouped_describe[label,'max'].iloc[1])
+        length = grouped_describe[label,'x_pos'].iloc[0] + grouped_describe[label,'x_pos'].iloc[1]
+        
+        test = ax.annotate("", 
+                    xy=(grouped_describe[label,'x_pos'].iloc[0], ylim), 
+                    xycoords='data',
+                    xytext=(grouped_describe[label,'x_pos'].iloc[1],ylim), 
+                    textcoords='data',
+                    arrowprops=dict(arrowstyle="-",
+                                    ec='#aaaaaa',
+                                    connectionstyle="bar,fraction=0.2",
+                                    linewidth=2))
+
+        #print(test) #Hold data if 2 take the same origin, put it higher ? 
+
+        # 0.5 is position of asterix
+        ax.text(length/2,
+                ylim + 5 ,
+                '*',
+                zorder=10,#order in z axis, so draw above all
+                horizontalalignment='center',
+                verticalalignment='center')
+
+
+
+        plt.ylim(0, ylim+50) # def lim depending of size of bar
+        
+
+
+    def draw_different(label1,label2, grouped_describe):
+        '''
+        create braket stars above all other because he link long distance bar
+        '''
+
+        ylim = max(grouped_describe[label1,'max'].iloc[0], grouped_describe[label2,'max'].iloc[1])
+        length = grouped_describe[label1,'x_pos'].iloc[0] + grouped_describe[label2,'x_pos'].iloc[1]
+        
+        ax.annotate("", 
+                    xy=(grouped_describe[label2,'x_pos'].iloc[0], ylim), 
+                    xycoords='data',
+                    xytext=(grouped_describe[label1,'x_pos'].iloc[1],ylim), 
+                    textcoords='data',
+                    arrowprops=dict(arrowstyle="-",
+                                    ec='#aaaaaa',
+                                    connectionstyle="bar,fraction=0.2",
+                                    linewidth=2))
+
+
+        # 0.5 is position of asterix
+        testo = ax.text(length/2,
+                ylim + 7,
+                '*',
+                zorder=10,#order in z axis, so draw above all
+                horizontalalignment='center',
+                verticalalignment='center')
+
+        print(testo)
+
+        plt.ylim(0, ylim+50) # def lim depending of size of bar
+        
+    statistic([a_bar, b_bar], variables_labels)
+    
+
+
+    draw('Age', grouped_describe)
+    draw('Force max de grip (N)', grouped_describe)
+
+    #draw_different('Force max de grip (N)','Age', grouped_describe)
+
+    
+    
+
+
+
+
+
+    '''
+    def barplot_annotate_brackets(num1, num2, data, center, height, yerr=None, dh=.05, barh=.05, fs=None, maxasterix=None):
+        """ 
+        Annotate barplot with p-values.
+        https://stackoverflow.com/questions/11517986/indicating-the-statistically-significant-difference-in-bar-graph
+
+        :param num1: number of left bar to put bracket over
+        :param num2: number of right bar to put bracket over
+        :param data: string to write or number for generating asterixes
+        :param center: centers of all bars (like plt.bar() input)
+        :param height: heights of all bars (like plt.bar() input)
+        :param yerr: yerrs of all bars (like plt.bar() input)
+        :param dh: height offset over bar / bar + yerr in axes coordinates (0 to 1)
+        :param barh: bar height in axes coordinates (0 to 1)
+        :param fs: font size
+        :param maxasterix: maximum number of asterixes to write (for very small p-values)
+        """
+
+        if type(data) is str:
+            text = data
+        else:
+            # * is p < 0.05
+            # ** is p < 0.005
+            # *** is p < 0.0005
+            # etc.
+            text = ''
+            p = .05
+
+            while data < p:
+                text += '*'
+                p /= 10.
+
+                if maxasterix and len(text) == maxasterix:
+                    break
+
+            if len(text) == 0:
+                text = 'n. s.'
+
+        lx, ly = center[num1], height[num1]
+        rx, ry = center[num2], height[num2]
+
+        if yerr:
+            ly += yerr[num1]
+            ry += yerr[num2]
+
+        ax_y0, ax_y1 = plt.gca().get_ylim()
+        dh *= (ax_y1 - ax_y0)
+        barh *= (ax_y1 - ax_y0)
+
+        y = max(ly, ry) + dh
+
+        barx = [lx, lx, rx, rx]
+        bary = [y, y+barh, y+barh, y]
+        mid = ((lx+rx)/2, y+barh)
+
+        plt.plot(barx, bary, c='grey')
+
+        kwargs = dict(ha='center', va='bottom')
+        if fs is not None:
+            kwargs['fontsize'] = fs
+
+        plt.text(*mid, text, **kwargs)
+
+
+    bars = np.arange(len(b_mean))
+    barplot_annotate_brackets(0, 1, '*', bars, b_mean)
+
+
+    bars = np.arange(len(a_mean))
+    barplot_annotate_brackets(0, 1, '*', bars, a_mean)
+    '''
+
+
+
+
+
+
+
+
+
+
+
+    '''
+    ##### EDIT THIS TO MAKE A FUNCTION MATCHING WITH
+    def correlation_bracket(pos_bar_1, pos_bar_2):
+        y_max = np.max(np.concatenate((a_mean, b_mean)))
+        y_min = np.min(np.concatenate((a_mean, b_mean)))
+        
+        # xy is the fist columns
+        # xytest is the second problem
+        # But here the columns are one for 2 (groued bar consider like 1)
+        # for xytext 2nd data is th offset
+
+        ax.annotate("", 
+                    xy=(0, y_max), 
+                    xycoords='data',
+                    xytext=(1, y_max), 
+                    textcoords='data',
+                    arrowprops=dict(arrowstyle="-",
+                                    ec='#aaaaaa',
+                                    connectionstyle="bar,fraction=0.2"))
+
+        # 0.5 is position of asterix
+        ax.text(0.5,
+                y_max + abs(y_max - y_min)*0.1,
+                '*',
+                horizontalalignment='center',
+                verticalalignment='center')
+
+
+
+        plt.ylim(0, 100) # def lim depending of size of bar
+    '''
+
+
+
+    fig.tight_layout()
+    plt.show()    
+    #savefig("1.svg") # or: "1.pdf" (depending on a backend)
--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
 
-import matplotlib as mpl
+
+## local paquages
+import graph_creator
+import run_stats
+
+
+## paquages
+import matplotlib.pyplot as plt
 import pandas as pd
 import inquirer
 
 from scipy import stats
 
-# Create variable to hold anwsers
+
+
+## Create dict to hold anwsers
 answers = {}
 
+
+# remove it later
+answers.update(file_path = '/home/yam/Downloads/TD3_etudiant.xls')
+
+
+
+'''
 ## Ask file to open
 questions = [
   inquirer.Path('file_path',
@@ -17,9 +33,8 @@ questions = [
                 ),
 ]
 answers.update(inquirer.prompt(questions))
+'''
 
-
-#file_path = '/home/yam/Downloads/TD3_etudiant.xls'
 
 
 ## Get excel file class
@@ -37,89 +52,168 @@ questions = [
 answers.update(inquirer.prompt(questions))
 
 
-
-## Select data spreadsheet
+## Select data spreadsheet and create df
 data = pd.read_excel(answers['file_path'], sheet_name=answers['spreadsheet'])
-
-
 print('This is your data :\n \n',data)
 
 
+#get the list of variables
 columns = data.columns.values.tolist()
 
-## Independent samples t-test ################################################
 
-## Ask grouping variable
+## Ask type of test
 questions = [
-    inquirer.List('grouping_variable',
-                message="Choose the grouping variable",
-                choices=columns,
+    inquirer.List('test_type',
+                message="Choose the statistic test you want to use",
+                choices=['Idependant T-Test', 'Dependant T-Test', 'No one'],
             ),
 ]
 
 answers.update(inquirer.prompt(questions))
-print('You choose :', answers['grouping_variable'])
-
-# Remove first choice from list
-columns.remove(answers['grouping_variable'])
+#print('You choose :', answers['test_type'])
 
 
-## Create group from the anwser
-grouped = data.groupby(answers['grouping_variable'])
-#grouped.group to obtain list of groups
-#print(grouped.get_group('Jeune'))## Obtain data of only one group
-
-print(grouped)
-
-'''
-## Statistical test
-for group in grouped:
-    print(group)
-'''
-
-tStat, pValue = stats.ttest_ind([grouped], equal_var = False) #run independent sample T-Test
-print("P-Value:{0} T-Statistic:{1}".format(pValue,tStat)) #print the P-Value and the T-Statistic
-
-## Levene Test for Equal Variances check random population choice in grouping before do statistics
-#stat, p = stats.levene(grouped) 
+## Option for just create braket (add grouping or not variables)
+if answers['test_type'] == 'No one':
+    ## Ask type of test
+    questions = [
+        inquirer.Checkbox('correlation_bracket',
+                    message="Choose a pair of variables you want to link",
+                    choices=columns
+                ),
+    ] 
 
 
+    answers.update(inquirer.prompt(questions))
+
+    ## Running steps using test choice
+elif answers['test_type'] == 'Idependant T-Test':
 
 
-'''
-Do the same for all the variables selected with for loop
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-## Ask for dependent variables
-questions = [
-  inquirer.Checkbox('dependent_variables',
-                    message="Choose the dependent variables (press space to select one or more)",
+    ## Ask grouping variable
+    questions = [
+        inquirer.List('grouping_variable',
+                    message="Choose the grouping variable",
                     choices=columns,
-                    ),
-]
+                ),
+    ]
 
-answers.update(inquirer.prompt(questions))
-print('You choose :', answers['dependent_variables'])
+    answers.update(inquirer.prompt(questions))
+
+    # Remove first choice from list
+    columns.remove(answers['grouping_variable'])
+
+
+    ## Create group from the anwser
+    grouped = data.groupby(answers['grouping_variable'])
+    #grouped.group to obtain list of groups
+    #print(grouped.get_group('Jeune'))## Obtain data of only one group
+
+
+
+    ## Ask type of relation (≠ or > or <)
+    questions = [
+        inquirer.List('relation',
+                    message="Choose the relation",
+                    choices=['A ≠ B', 'A > B', 'B < A'],
+                ),
+    ]
+
+    answers.update(inquirer.prompt(questions))
 
 
 
 
-################################################
-## Ask for descriptive statistics
-descriptive = describe(v) # where v is the data columns
+    ## Ask wich one is group A and wich one is group B 
+    questions = [
+        inquirer.List('a_group',
+                    message="Choose the groupe A",
+                    choices=grouped.groups.keys(),
+                ),
+    ]
 
-print(answers)
+    answers.update(inquirer.prompt(questions))
+
+
+
+
+    ## Ask for dependent variables
+    questions = [
+    inquirer.Checkbox('dependent_variables',
+                        message="Choose the dependent variables (press space to select one or more)",
+                        choices=columns,
+                        ),
+    ]
+
+    answers.update(inquirer.prompt(questions))
+
+
+    ## Create b group label from the A choose
+    b_group = list(grouped.groups.keys())
+    b_group.remove(answers['a_group'])
+
+    ## Get separate data from the result
+    a_group = grouped.get_group(answers['a_group'])
+    b_group = grouped.get_group(b_group[0])
+
+
+    ## T- Test
+    test_results = {}
+
+    test_results['a_mean'] = []
+    test_results['b_mean'] = []
+    test_results['a_std'] = []
+    test_results['b_std'] = []
+
+    for variable in answers['dependent_variables']:
+        
+        # T-Test
+        test_results[variable] = stats.ttest_ind(a_group[variable], b_group[variable])
+
+        ## mean and std hand
+        test_results['a_mean'].append(a_group[variable].mean())
+        test_results['b_mean'].append(b_group[variable].mean())
+        test_results['a_std'].append(a_group[variable].std())
+        test_results['b_std'].append(b_group[variable].std())
+
+
+    graph_creator.grouped_bar_plot(test_results['a_mean'], 
+                                    test_results['b_mean'], 
+                                    test_results['a_std'], 
+                                    test_results['b_std'], 
+                                    list(grouped.groups), 
+                                    list(answers['dependent_variables']),
+                                    grouped.describe())
+    #
+    #graph_creator.grouped_bar_plot(test_results['mean'], test_results['std'], a_group.columns.values.tolist())
+
+
+
+
+    ## Levene Test for Equal Variances check random population choice in grouping before do statistics
+    #stat, p = stats.levene(grouped) 
+
+
+
+
+    ################################################
+    ## Ask for descriptive statistics
+    # Do you want an descriptive tables ? 
+    # if yes, wich data you want ? (mean, std, quartiles, etc)
+
+    ## Directly create descriptive stats image
+    describe = grouped.describe()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -143,17 +237,6 @@ answers = inquirer.prompt(questions)
 
 
 
-## Ask wich column have to use
-choice = input("Type 1 for bar plot:")
-
-questions = [
-    inquirer.List('graph_type',
-                message="Which type of graph you want ? ",
-                choices=['bar plot', 'line'],
-            ),
-]
-answers = inquirer.prompt(questions)
-print(answers)
 
 
 
